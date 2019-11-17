3fd1af7de8c4f6a267306d18fe04c0df1f950fe3
Christina Golovach <christina.golovach@gmail.com>
2019-11-17 20:20:04 +0300

    module5_ complete  analizer

--- Potestas/Potestas/Analizers/SqlORMAnalizer.cs
+++ Potestas/Potestas/Analizers/SqlORMAnalizer.cs
@@ -1,15 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Potestas.Mapper;
 using Potestas.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace Potestas.Analizers
 {
     public class SqlORMAnalizer : IEnergyObservationAnalizer
     {
         private readonly DbContext _dbContext;
+
         public SqlORMAnalizer(DbContext dbContext)
         {
             _dbContext = dbContext ?? throw new ArgumentNullException($"the {nameof(dbContext)} can not be null.");
@@ -37,17 +38,30 @@ namespace Potestas.Analizers
 
         public IDictionary<Coordinates, int> GetDistributionByCoordinates()
         {
-            throw new NotImplementedException();
+            var ORMDistribution = _dbContext.Set<EnergyObservations>().Include(obs => obs.Coordinate)
+                                                                    .GroupBy(obs => obs.Coordinate)                                                          
+                                                                    .ToDictionary(k => k.Key, v => v.Count());
+
+            var domainDistribution = new Dictionary<Coordinates, int>();
+
+            foreach (var ormEntity in ORMDistribution)
+            {
+                domainDistribution.Add(ormEntity.Key.ToDomainEntity(), ormEntity.Value);
+            }
+
+            return domainDistribution;
         }
 
         public IDictionary<double, int> GetDistributionByEnergyValue()
         {
-            throw new NotImplementedException();
+            return _dbContext.Set<EnergyObservations>().GroupBy(obs => obs.EstimatedValue)
+                                                       .ToDictionary(k => k.Key, v => v.Count());
         }
 
         public IDictionary<DateTime, int> GetDistributionByObservationTime()
         {
-            throw new NotImplementedException();
+            return _dbContext.Set<EnergyObservations>().GroupBy(obs => obs.ObservationTime)
+                                                       .ToDictionary(k => k.Key, v => v.Count());
         }
 
         public double GetMaxEnergy()
@@ -58,47 +72,64 @@ namespace Potestas.Analizers
         public double GetMaxEnergy(Coordinates coordinates)
         {
             return _dbContext.Set<EnergyObservations>().Where(obs => obs.CoordinateId == coordinates.Id)
-                                                       .Average(obs => obs.EstimatedValue);
+                                                       .Max(obs => obs.EstimatedValue);
         }
 
         public double GetMaxEnergy(DateTime dateTime)
         {
-            throw new NotImplementedException();
+            return _dbContext.Set<EnergyObservations>().Where(obs => obs.ObservationTime == dateTime)
+                                                       .Max(obs => obs.EstimatedValue);
         }
 
         public Coordinates GetMaxEnergyPosition()
         {
-            throw new NotImplementedException();
+            var ORMCoordinates =  _dbContext.Set<EnergyObservations>().Include(obs => obs.Coordinate)
+                                                                      .OrderByDescending(obs => obs.EstimatedValue) 
+                                                                      .AsQueryable()
+                                                                      .First().Coordinate;
+
+            return ORMCoordinates.ToDomainEntity();
         }
 
         public DateTime GetMaxEnergyTime()
         {
-            throw new NotImplementedException();
+            return _dbContext.Set<EnergyObservations>().OrderByDescending(obs => obs.EstimatedValue)
+                                                       .AsQueryable()
+                                                       .First().ObservationTime;
         }
 
         public double GetMinEnergy()
         {
-            throw new NotImplementedException();
+            return _dbContext.Set<EnergyObservations>().Min(obs => obs.EstimatedValue);
         }
 
         public double GetMinEnergy(Coordinates coordinates)
         {
-            throw new NotImplementedException();
+            return _dbContext.Set<EnergyObservations>().Where(obs => obs.CoordinateId == coordinates.Id)
+                                                       .Min(obs => obs.EstimatedValue);
         }
 
         public double GetMinEnergy(DateTime dateTime)
         {
-            throw new NotImplementedException();
+            return _dbContext.Set<EnergyObservations>().Where(obs => obs.ObservationTime == dateTime)
+                                                       .Min(obs => obs.EstimatedValue);
         }
 
         public Coordinates GetMinEnergyPosition()
         {
-            throw new NotImplementedException();
+            var ORMCoordinates = _dbContext.Set<EnergyObservations>().Include(obs => obs.Coordinate)
+                                                                      .OrderBy(obs => obs.EstimatedValue)
+                                                                      .AsQueryable()
+                                                                      .First().Coordinate;
+
+            return ORMCoordinates.ToDomainEntity();
         }
 
         public DateTime GetMinEnergyTime()
         {
-            throw new NotImplementedException();
+            return _dbContext.Set<EnergyObservations>().OrderBy(obs => obs.EstimatedValue)
+                                                       .AsQueryable()
+                                                       .First().ObservationTime;
         }
     }
 }
---
+++ Potestas/Potestas/Mapper/EnergyObservationsMapper.cs
@@ -0,0 +1,18 @@
+﻿using Potestas.Models;
+
+namespace Potestas.Mapper
+{
+    public static class EnergyObservationsMapper
+    {
+        public static EnergyObservations ToORMEntity(this IEnergyObservation energyObservation)
+        {
+            return new EnergyObservations()
+            {
+                Id = energyObservation.Id,
+                CoordinateId = energyObservation.ObservationPoint.Id,
+                EstimatedValue = energyObservation.EstimatedValue,
+                ObservationTime = energyObservation.ObservationTime
+            };
+        }
+    }
+}
