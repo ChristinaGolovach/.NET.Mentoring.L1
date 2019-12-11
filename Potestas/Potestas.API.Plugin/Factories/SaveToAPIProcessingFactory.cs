eaf9202b5aab2eca4e47f9c1f106bcb82510d55e
Krystsina Halavach <Krystsina_Halavach@epam.com>
2019-12-11 14:11:04 +0300

    task_2 add skeleton

---
+++ Potestas/Potestas.API.Plugin/Factories/SaveToAPIProcessingFactory.cs
@@ -0,0 +1,33 @@
+﻿using Potestas.API.Plugin.Analizers;
+using Potestas.API.Plugin.Storages;
+using Potestas.Processors;
+
+namespace Potestas.API.Plugin.Factories
+{
+    public class SaveToAPIProcessingFactory : IProcessingFactory<IEnergyObservation>
+    {
+        private IEnergyObservationStorage<IEnergyObservation> _storage;
+
+        public IEnergyObservationAnalizer CreateAnalizer() 
+            => new APIAnalizer();
+
+
+        public IEnergyObservationProcessor<IEnergyObservation> CreateProcessor()
+            => new SaveToStorageProcessor<IEnergyObservation>(GetStorage());
+
+
+        public IEnergyObservationStorage<IEnergyObservation> CreateStorage() 
+            => GetStorage();
+
+
+        private IEnergyObservationStorage<IEnergyObservation> GetStorage()
+        {
+            if (_storage == null)
+            {
+                _storage = new APIStorage<IEnergyObservation>();
+            }
+
+            return _storage;
+        }
+    }
+}
---
+++ Potestas/Potestas.API.Plugin/Storages/APIStorage.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Potestas.API.Plugin.Storages
+{
+    public class APIStorage<T> : IEnergyObservationStorage<T> where T : IEnergyObservation
+    {
+        public string Description => throw new NotImplementedException();
+
+        public int Count => throw new NotImplementedException();
+
+        public bool IsReadOnly => throw new NotImplementedException();
+
+        public void Add(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    //https://medium.com/cheranga/calling-web-apis-using-typed-httpclients-net-core-20d3d5ce980
+    //http://zetcode.com/csharp/httpclient/
+    //https://docs.microsoft.com/en-us/aspnet/web-api/overview/advanced/calling-a-web-api-from-a-net-client
+}
